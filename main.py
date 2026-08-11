29a4be6ffd3409b614e832d4ee2bff0bb0b4d17e
Ali Raza <itzharj@gmail.com>
2026-08-11 16:23:47 +0500

    Exploratory Data Analysis and Filter Data with Pandas

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+import pandas as pd 
+
+
+# Understand the given data 
+read_csv = pd.read_csv(r"data_set\electricity_cost_dataset.csv") 
+print(read_csv.head())
+print(read_csv.describe())
+print(read_csv.info())
+print(read_csv.columns)
+
+# Data Filtering
+find_missing_values = read_csv.isnull().sum()
+find_duplicate_value = read_csv.duplicated().sum()
+print(find_duplicate_value)
+
+
+
